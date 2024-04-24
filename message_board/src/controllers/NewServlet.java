9d082572dd5a99136c926cad0978d880ca1d1b2a
dir-FY24kamadashun <shun.kamada.f332@dirbato.co.jp>
2024-04-24 11:20:02 +0900

    Index/NewServletの修正

--- message_board/src/controllers/NewServlet.java
+++ message_board/src/controllers/NewServlet.java
@@ -6,13 +6,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.persistence.EntityManager;
-import java.sql.Timestamp;
 
 
 
 import models.Message;
-import utils.DBUtil;
+
 
 /**
  * Servlet implementation class NewServlet
@@ -34,30 +32,10 @@ public class NewServlet extends HttpServlet {
 	 */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
-
-        // Messageのインスタンスを生成
-        Message m = new Message();
-
-        // mの各フィールドにデータを代入
-        String title = "taro";
-        m.setTitle(title);
-
-        String content = "hello";
-        m.setContent(content);
-
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis()); // 現在の日時を取得
-        m.setCreated_at(currentTime);
-        m.setUpdated_at(currentTime);
-
-        // データベースに保存
-        em.persist(m);
-        em.getTransaction().commit();
-
-        // 自動採番されたIDの値を表示
-        response.getWriter().append(Integer.valueOf(m.getId()).toString());
-
-        em.close();
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("message",new Message());
+       var rd =request.getRequestDispatcher("/WEB-INF/view/messages/new.jsp");
+       rd.forward(request, response);
     }
+        
 }
